f8140f3ab71d986440312fcee70caa525dbfc779
Paola Guarasci <paolaguarasci@gmail.com>
2016-08-01 16:46:31 +0200

    Aggiunta programmazione e commenti in ogni file

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// TODO
+//  - Colore di sfondo dinamico (random) inserito come proprietà dell'oggetto;
+//  - Il cambio di colore avvine lentamente (fade in-out);
+//  - Icone di tumblr e twitter e pulsante "Nuova citazione" dello stesso colore dello sfondo;
+
+// --- PSEUDOCODE ---
+// Parse del JSON
+// Generazione di un numero casuale tra 1 e "lunghezza JSON"
+// Cambio dinamico del colore dei pulsanti
+// Cambio dinamico del colore dello sfondo
+// Cambio dinamico del colore delle icona
+// Trigger sul pulsante per generare una nuova citazione
+// Pulsanti di condivisione che "inviano" la citazione
+// Stampa dell'elemento con id X (generato casualmente)
